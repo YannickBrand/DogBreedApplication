a4f4a07050a15940a77ff9dd800dd4bdfaa22bb6
YannickBrand <yannick.brand45@gmail.com>
2023-03-05 01:46:57 +0100

    Eerste test dogsapi

--- dogbreedapplication/src/App.js
+++ dogbreedapplication/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import React, { Component }  from 'react'
 import Home from "./pages/Home"
-import SingleDog from "./pages/SingleDog"
+import SingleDog from "./pages/SingleDog";
 
 function App() {
   return (
--- dogbreedapplication/src/pages/Home.js
+++ dogbreedapplication/src/pages/Home.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    const [dogs, setDogs] = useState([])
+
+    useEffect(() =>
+    {
+const fetchDogData = async() => {
+  try {
+    
+    const res = await fetch("https://api.thedogapi.com/v1/breeds/")
+    const data = await res.json()
+    setDogs(data)
+    console.log(data)
+    
+  } catch(error) {
+    console.error(error)
+  } 
+}
+fetchDogData()
+},[])
+
+  return <div>Home</div>
 }
