47a837546bd06f541c63728480373ccf2cbee4dc
YannickBrand <yannick.brand45@gmail.com>
2023-03-05 23:17:05 +0100

    Test api display
    tailwind en api test

--- dogbreedapplication/src/pages/Home.js
+++ dogbreedapplication/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 
-
 export default function Home() {
     const [dogs, setDogs] = useState([])
 
@@ -18,8 +17,26 @@ const fetchDogData = async() => {
     console.error(error)
   } 
 }
+
+
 fetchDogData()
 },[])
 
-  return <div>Home</div>
+  return ( 
+  <>
+  {!dogs ? (
+  <h1 className='flex items-center justify-center text-slate-800
+   text-center px-5 text-3xl h-screen font-bold uppercase'>
+    Loading...
+    </h1> 
+    ) : (
+    <>
+  <section className='p-8 max-w-6-xl mx-auto'>
+    <h1 className='flex items-center justify-center text-slate-100
+   text-center px-5 text-3xl h-screen font-bold ' >The Dog App</h1>
+    {dogs.length} dogs    </section>
+    </>
+    )}
+    </>
+    )
 }
